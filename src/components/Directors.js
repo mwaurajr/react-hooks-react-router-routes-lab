8ca7c231732c8fe20413a81c8d9d0bfd6a041bc4
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-09-30 13:11:36 +0300

    completed react routers

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorList= directors.map((dir)=>{
+    //console.log('math ' + Math.floor(Math.random()  * (1000 - 100)) + 100)
+    // .map populates new array the results showing the called function in the calling array
+    // math.floor => rounds off to the nearest integer
+    // math.random =>  returns a random number that's greater than or equal to 0 and less than 1
+    const moviesList = (dir.movies).map((mov)=><li key={Math.floor(Math.random()  * (1000 - 100)) + 100}>{mov}</li>)
+      return (
+        <div>
+           <h1>{dir.name}</h1>
+           <ul>
+             {moviesList}
+           </ul>
+        </div>)                                 
+  })
+
+  return (
+      <div>
+        <h1>Directors Page</h1>
+        {directorList}
+      </div>
+  )
 }
 
 export default Directors;
